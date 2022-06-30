3df0c80424fadd5fd9d219af157dea6bd0062a53
Max Bobrov <maxbobrov1997@gmail.com>
2022-06-30 13:52:13 +0300

    desktop v

--- js/index.js
+++ js/index.js
@@ -2,5 +2,9 @@
 $(document).ready(function () {
     $('#getStarted').click(function () {
         $('#youGet').removeClass('hidden');
-      }); 
+    });
+    $('#sendForm').click(function () {
+        $('#beforeSent').addClass('hidden');
+        $('#sent').removeClass('hidden');
+    });
 });
